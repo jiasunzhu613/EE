67193f069e00de12ab8bbddeeb5431cd0b5e36a1
jonathanzhu <jiasunzhu@gmail.com>
2023-08-17 12:19:17 -0400

    Changed fenwick implementation to properly obtain substring hashes

--- main.py
+++ main.py
@@ -1,65 +1,183 @@
-import random, math
+# Fast IO
+from sys import stdin
+input = stdin.readline
 
-# MODIFIED FENWICK FUNCTIONS
-def LSB(i):
-    return i & -i
+"""
+Main concepts used: polynomial hash, Fenwick tree (although this could also be implemented with a segment tree)
 
-#TODO: construct method causing overflow errors
-def construct(arr): # where arr is a 1 indexed array of numbers
+Polynomial hash explained:
+- A polynomial hash is a method of string hashing that only uses multiplication and addition
+
+- A general polynomial hash follows the pattern: (a*P^N-1 + b*P^N-2 + c*P^N-3...x*P^2 + y*P^1 + z*P^0) % M 
+     -> (letters all represent some number, whether it be actual numbers or ASCII codes of letters)
+     -> P is some prime base for the hash function
+        -> **some unique mathematical things regarding P needs research for understanding 
+        regarding the increase of the base for less hash collisions**
+     -> M is some large prime to mod the end value by 
+        -> M >>>>> P (M should be much bigger than P)
+        
+- There are many ways to generate a polynomial hash:
+    -> One way is through the use of a sliding window to create a rolling hash:
+        - from the above equation for a generic polynomial hash
+          we can factor it to see that it also follows: z + P(y + P(x + P(...b + P(a))))
+            -> from this the rolling hash can be broken down into a sequence of multiplications and additions
+                -> more specifically this sequence:
+                    hash *= P
+                    hash += val (where val is the number value we want to add to our hash)
+                    
+    -> Another way is to use prefix sums/prefix hashes: 
+        - say we want to generate a polynomial hash for the substring from index i to j
+            -> hash needed: a_1 * P^j-1 + a_2 * P^j-2 + a_3*P^j-3 .., a_i*P^0
+               hash of ind 1 to j: a_1 * P^j-1 + a_2 * P^j-2 + a_3*P^j-3 .., a_j*P^0
+               hash of ind 1 to i - 1: a_1 * P^i-1 + a_2 * P^i-2 + a_3*P^i-3 .., a_i-1*P^0
+               
+               since i exists before j, we can substract the hash of i - 1 from j to obtain a hash for i to j
+               Formula generalizes to: H(i,j) = (H[1, j]-H[1, i-1]*B^(j-i+1) ) % MOD
+               
+        - this method is more easily understood through an example:
+            -> say we have the array [1, 2, 3] we want to hash
+            
+            goal: obtain hash for index 2 to 3
+            
+            target hash: 2*P^1 + 3*P^0
+            prefix hash of 3: 1*P^2 + 2*P^1 + 3*P^0
+            prefix hash of 2-1 (1): 1*P^0
+            
+            1*P^2 + 2*P^1 + 3*P^0 - (1*P^0 * P^3-2+1) = 1*P^2 + 2*P^1 + 3*P^0 - 1*P^2 = 2*P^1 + 3*P^0
+            
+            
+        - Resources: https://codeforces.com/blog/entry/18407 / https://codeforces.com/blog/entry/60445
+        
+        
+Main ideas of implementation explained:
+- Each index i will hold a partial hash for elements from index i - LSB(i) to i
+    -> for example:
+        - index 4 (0100 in binary) will hold a partial hash for elements from index 0 to 4
+        - index 10 (1010 in binary) will hold a partial hash for elements from index 8 (10 - 2) to 10 
+        
+    -> how do we construct our tree?
+        - say we have array [1, 2, 3, 4]
+        (| bars represent areas of responsibility, for more info: https://www.youtube.com/watch?v=BHPez138yX8)
+        4         | 1*P^3 + 2*P^2 + 3*P^1 + 4*P^0
+        3 011 |   | 3*P^0
+        2 010   | | 1*P^1 + 2*P^0
+        1 001 | | | 1*P^0
+        
+        How can we construct the above tree?
+            -> generic fenwick construction adds the value of a current ind to the value at index ind + LSB(ind)
+            -> we can follow the same construction:
+                -> tree initially starts off as the unchanged array (though it would be 1 indexed)
+                -> for each iteration in construction, we add fenwick[i] * P^LSB(i) to fenwick[i + LSB(i)]
+        
+        
+- Using the partial hashes, we can generate a prefix hash for any index and use that prefix hash 
+and the formula explained in the polynomial hash section to generate any hash we want
+    -> How can we generate prefix hash from partial hash?
+    
+    yet again this is better explained through an example:
+    
+    4         | 1*P^3 + 2*P^2 + 3*P^1 + 4*P^0
+    3 011 |   | 3*P^0
+    2 010   | | 1*P^1 + 2*P^0
+    1 001 | | | 1*P^0
+    
+    - for each iteration in summing:
+        -> generic fenwick prefix sums are achieved by adding all fenwick values of the desired index's active bits
+        -> multiplier variable set to 0 at first
+        -> add current current fenwick[i] * P^multiplier to total
+        -> add LSB(i) to multiplier
+    
+    example using 3:
+    target hash: 1 * P^2 + 2*P^1 + 3*P^0
+    
+    summing using fenwick:
+    -> add fenwick[3] * P^multiplier(0 currently) -> 1(3*P^0) = 3*P^0
+    -> add LSB(3) to multiplier (1 after update)
+    -> subtract LSB(3) from 3 -> turns to 2
+    -> add fenwick[2] * P^multiplier(1 currently) -> P(1*P^1 + 2*P^0) = 1 * P^2 + 2*P^1
+    -> total = 1 * P^2 + 2*P^1 + 3*P^0
+"""
+
+# Fenwick implementation
+# Function to get least significant bit (LSB)
+def LSB(x):
+    return x & -x
+
+# Function to construct fenwick tree to maintain polynomial hash
+def construct(arr):
     fenwick = arr.copy()
     for i in range(1, len(fenwick)):
-        lsb = LSB(i)
-        j = i + lsb
+        j = i + LSB(i)
         if j < len(fenwick):
-            fenwick[j] += fenwick[i] * pow(P, lsb, M)
+            fenwick[j] += (fenwick[i] * pow(P, LSB(i), M)) % M
             fenwick[j] %= M
     return fenwick
 
-def range_sum(fenwick, i):
+# Function to get prefix sums from fenwick tree
+def prefix_sum(fenwick, pos):
     tot = 0
-    lsb_counter = 0
-    while i != 0:
-        tot += fenwick[i]
+    iteration = 0
+    while pos > 0:
+        tot += (fenwick[pos] * pow(P, iteration, M)) % M # might need mod after
         tot %= M
-        i -= LSB(i)
+        iteration += LSB(pos)
+        pos -= LSB(pos)
     return tot
 
-#TODO: problems caused by modding and dividing, once a number is modded it is no longer a direct multiple of P
-def interval_sum(fenwick, i, j):
-    #prolly shouldnt divide // pow(P, min(i, j) - 1)
-    return ((range_sum(fenwick, j) - range_sum(fenwick, i - 1))// pow(P, min(i, j) - 1)) % M
+# Function to get range sums from fenwick tree using prefix sums
+def query(fenwick, l ,r):
+    global P, M
+    return (prefix_sum(fenwick, r) - prefix_sum(fenwick, l - 1) * pow(P, r-l+1, M)) % M
 
-def update(fenwick, ind, diff):
-    while ind < len(fenwick):
-        fenwick[ind] += diff
-        fenwick[ind] %= M
-        ind += LSB(ind)
+# Function to update values in the fenwick tree
+def update(fenwick, pos, diff):
+    iteration = 0
+    while pos < len(fenwick):
+        fenwick[pos] += (diff * pow(P, iteration, M)) % M
+        fenwick[pos] %= M
+        iteration += LSB(pos)
+        pos += LSB(pos)
     return
 
 # SOLVE
-P = 27
+P = 209389283097
 M = 10**9 + 7
 N, Q = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
-new_arr = arr.copy()
-# for i in range(1, N + 1):
-#     arr[i] *= pow(P, i - 1, M)
 fenwick = construct(arr)
 
 for _ in range(Q):
-    query = list(map(int, input().split()))
-    if query[0] == 1:
-        print(fenwick)
-        print(interval_sum(fenwick, query[1], query[2]))
-        print(interval_sum(fenwick, query[3], query[4]))
-        print(int(interval_sum(fenwick, query[1], query[2]) == interval_sum(fenwick, query[3], query[4])))
+    q = list(map(int, input().split()))
+    if q[0] == 1:
+        l1, r1, l2, r2 = q[1:]
+        print(int(query(fenwick, l1, r1) == query(fenwick, l2, r2)))
     else:
-        update(fenwick, query[1], abs(query[2]*pow(P, query[1], M) - arr[query[1]]))
-        arr[query[1]] = query[2]*pow(P, query[1], M)
+        i, v = q[1:]
+        update(fenwick, i, v - arr[i])
+        arr[i] = v
 
-# print(arr)
-# print(fenwick)
-# print(interval_sum(fenwick, 1, 1))
-# print(interval_sum(fenwick, 3, 3))
-# print(interval_sum(fenwick, 1, 2))
-# print(interval_sum(fenwick, 3, 4))
+
+"""
+5 4
+1 3 1 1 1
+1 1 5 1 5
+1 1 3 3 5
+2 3 4
+1 1 3 3 5
+
+10 1
+4 6 5 5 9 3 9 5 1 8
+1 3 3 8 8
+
+5 2
+1 2 3 2 5
+1 2 2 4 4
+
+True False
+1 6 8 1 7 3 2 1 5 5
+8 8 1 1
+7 2
+6 3
+2 6
+"""
+
--- randomTesting.py
+++ randomTesting.py
@@ -1,34 +1,41 @@
 import random, math
 
-# MODIFIED FENWICK FUNCTIONS
-def LSB(i):
-    return i & -i
+# Fenwick implementation
+def LSB(x):
+    return x & -x
 
-#TODO: construct method causing overflow errors
-def construct(arr): # where arr is a 1 indexed array of numbers
+def construct(arr):
     fenwick = arr.copy()
     for i in range(1, len(fenwick)):
         j = i + LSB(i)
         if j < len(fenwick):
-            fenwick[j] += fenwick[i] % M
+            fenwick[j] += (fenwick[i] * pow(P, LSB(i), M)) % M
+            fenwick[j] %= M
     return fenwick
 
-def range_sum(fenwick, i):
+def prefix_sum(fenwick, pos):
     tot = 0
-    while i != 0:
-        tot += fenwick[i] % M
-        i -= LSB(i)
+    iteration = 0
+    while pos > 0:
+        tot += (fenwick[pos] * pow(P, iteration, M)) % M # might need mod after
+        tot %= M
+        iteration += LSB(pos)
+        pos -= LSB(pos)
     return tot
 
-#TODO: problems caused by modding and dividing, once a number is modded it is no longer a direct multiple of P
-def interval_sum(fenwick, i, j):
-    #// pow(P, min(i, j) - 1)
-    return ((range_sum(fenwick, j) - range_sum(fenwick, i - 1))// pow(P, min(i, j) - 1)) % M
+# prefix_sum(r) - prefix_sum(l) - prefix_sum(l - 1)* pow(P, r-l+1) % M
+def query(fenwick, l ,r):
+    global P, M
+    return (prefix_sum(fenwick, r) - prefix_sum(fenwick, l - 1) * pow(P, r-l+1, M)) % M
+# * (pow(P, r - l + 1) + 1)
 
-def update(fenwick, ind, diff):
-    while ind < len(fenwick):
-        fenwick[ind] += diff % M
-        ind += LSB(ind)
+def update(fenwick, pos, diff):
+    iteration = 0
+    while pos < len(fenwick):
+        fenwick[pos] += (diff * pow(P, iteration, M)) % M
+        fenwick[pos] %= M
+        iteration += LSB(pos)
+        pos += LSB(pos)
     return
 
 
@@ -38,30 +45,32 @@ M = 10**9+7
 for _ in range(100):
     N, Q = 10, random.randint(5, 10)
     arr = [0] + [random.randint(1, 10) for i in range(N)]
-    new_arr = arr.copy()
-    for i in range(1, N + 1):
-        arr[i] *= pow(P, i - 1, M)
+    pre_change = arr.copy()
+    updates = []
     fenwick = construct(arr)
 
     for _ in range(Q):
-        type = random.randint(1, 3)
+        type = random.randint(1, 2)
         if type == 1:
-            a = random.randint(1, N - 1)
-            b = random.randint(a, N)
-            c = random.randint(1, N - 1)
-            d = random.randint(c, N)
+            length = random.randint(1, 9)
+            a = random.randint(1, N - length)
+            b = a + length
+            c = random.randint(1, N - length)
+            d = c + length
             def slow():
-                return new_arr[a:b + 1] == new_arr[c:d+1]
+                return arr[a:b + 1] == arr[c:d+1]
             def fast():
-                return interval_sum(fenwick, a, b) == interval_sum(fenwick, c, d)
+                return query(fenwick, a, b) == query(fenwick, c, d)
             if slow() != fast():
                 print(slow(), fast())
-                print(*new_arr[1:])
+                print(*pre_change[1:])
                 print(a, b, c, d)
+                for i in updates:
+                    print(*i)
                 print()
         else:
             a = random.randint(1, N)
             b = random.randint(1, 10)
-            update(fenwick, a, abs(b*pow(P, a, M) - arr[a]))
-            arr[a] = b*pow(P, a, M)
-            new_arr[a] = b
+            updates.append((a, b))
+            update(fenwick, a, b - arr[a])
+            arr[a] = b
